674c7086ac77c80b4aab07d69f317c88884c7d4b
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-03-27 12:17:54 +0200

    No styles yet

--- src/main/java/dk/pokedex/controller/MyController.java
+++ src/main/java/dk/pokedex/controller/MyController.java
@@ -1,2 +1,96 @@
-package dk.pokedex.controller;public class MyController {
+package dk.pokedex.controller;
+
+import dk.pokedex.model.Pokedex;
+import dk.pokedex.repository.PokedexRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+    PokedexRepository pokedexRepository;
+    public MyController(PokedexRepository pokedexRepository){
+        this.pokedexRepository = pokedexRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("pokedex", pokedexRepository.getAll());
+        return "index";
+    }
+    //From anchor in index
+    @GetMapping("/create")
+    public String showCreate(){
+        return "create";
+    }
+    //From form action - if you have autoincrement you don't need to set that specific value
+    @PostMapping("/create")
+    public String addPokemon(@RequestParam("pokedex_number") int pokedex_number,
+                             @RequestParam("name-field") String name_field,
+                             @RequestParam("speed") int speed,
+                             @RequestParam("special_defence") int special_defence,
+                             @RequestParam("special_attack") int special_attack,
+                             @RequestParam("defence") int defence,
+                             @RequestParam("attack") int attack,
+                             @RequestParam("hp") int hp,
+                             @RequestParam("primary_type") String primary_type,
+                             @RequestParam("secondary_type") String secondary_type){
+        Pokedex newPokedex = new Pokedex();
+        newPokedex.setPokedex_number(pokedex_number);
+        newPokedex.setName(name_field);
+        newPokedex.setSpeed(speed);
+        newPokedex.setSpecial_defence(special_defence);
+        newPokedex.setSpecial_attack(special_attack);
+        newPokedex.setDefence(defence);
+        newPokedex.setAttack(attack);
+        newPokedex.setHp(hp);
+        newPokedex.setPrimary_type(primary_type);
+        newPokedex.setSecondary_type(secondary_type);
+
+        pokedexRepository.addPokemon(newPokedex);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{pokedex_number}")
+    public String showUpdate(@PathVariable("pokedex_number") int update_pokedex_number, Model model){
+        //Finding produkt with pokedex_number = update_pokedex_number
+        Pokedex pokedexUpdate = pokedexRepository.findPokemonByNumber(update_pokedex_number);
+
+        //Add a pokemon to the viewmodel so it can be used by Thymeleaf
+        model.addAttribute("pokedex", pokedexUpdate);
+
+        //Tell spring which html page has to be shown
+        return "update";
+    }
+    @PostMapping("/update")
+    public String updatePokemon(@RequestParam("pokedex_number")int update_pokedex_number,
+                                @RequestParam("name-field") String update_name_field,
+                                @RequestParam("speed") int update_speed,
+                                @RequestParam("special_defence") int update_special_defence,
+                                @RequestParam("special_attack") int update_special_attack,
+                                @RequestParam("defence") int update_defence,
+                                @RequestParam("attack") int update_attack,
+                                @RequestParam("hp") int update_hp,
+                                @RequestParam("primary_type") String update_primary_type,
+                                @RequestParam("secondary_type") String update_secondary_type){
+        Pokedex updatePokedex = new Pokedex(update_pokedex_number, update_name_field, update_speed, update_special_defence,
+                                            update_special_attack, update_defence, update_attack, update_hp,
+                                            update_primary_type, update_secondary_type);
+
+        pokedexRepository.updatePokedex(updatePokedex);
+
+        return "redirect:/";
+    }
+    @GetMapping("/delete/{pokedex_number}")
+    public String deletePokemon(@PathVariable("pokedex_number") int pokedex_number){
+        //Call respository to delete from it
+        pokedexRepository.deletePokemonNumber(pokedex_number);
+
+        //Redirect to index page
+        return "redirect:/";
+    }
 }
--- src/main/java/dk/pokedex/repository/PokedexRepository.java
+++ src/main/java/dk/pokedex/repository/PokedexRepository.java
@@ -1,2 +1,177 @@
-package dk.pokedex.repository;public class PokedexRepository {
+package dk.pokedex.repository;
+
+import dk.pokedex.model.Pokedex;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PokedexRepository {
+    //private final static String DB_URL = "jdbc:mysql://mhkmysql.mysql.database.azure.com:3306/pokedex";
+    //private final static String UID = "MKaae";
+    //private static String PWD = "25cjYcyOoz1ifa";
+
+    //Database properties injected from application.properties
+    @Value("${spring.datasource.url}")
+    private String DB_URL;
+    @Value("${spring.datasource.username}")
+    private String UID;
+    @Value("${spring.datasource.password}")
+    private String PWD;
+
+    public List<Pokedex> getAll(){
+        List<Pokedex> pokedexList = new ArrayList<>();
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            Statement statement = connection.createStatement();
+            final String SQL_QUERY = "SELECT * FROM pokemon";
+            ResultSet resultSet = statement.executeQuery(SQL_QUERY);
+            while(resultSet.next()) {
+                int pokedex_number = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                int speed = resultSet.getInt(3);
+                int special_defence = resultSet.getInt(4);
+                int special_attack = resultSet.getInt(5);
+                int defence = resultSet.getInt(6);
+                int attack = resultSet.getInt(7);
+                int hp = resultSet.getInt(8);
+                String primary_type = resultSet.getString(9);
+                String secondary_type = resultSet.getString(10);
+                Pokedex pokedex = new Pokedex(pokedex_number, name, speed, special_defence, special_attack,
+                    defence, attack, hp, primary_type, secondary_type);
+                pokedexList.add(pokedex);
+
+            }
+
+        } catch(SQLException SQLE){
+            System.out.println("There was no connection found.");
+            SQLE.printStackTrace();
+        }
+        return pokedexList;
+    }
+    public void addPokemon(Pokedex pokedex){
+        try{
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+
+            final String CREATE_QUERY = "INSERT INTO pokemon(pokedex_number, name, speed, special_defence," +
+                    "special_attack, defence, attack, hp, primary_type, secondary_type) VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(CREATE_QUERY);
+
+            //Set attributes in prepared statement
+            preparedStatement.setInt(1, pokedex.getPokedex_number());
+            preparedStatement.setString(2, pokedex.getName());
+            preparedStatement.setInt(3, pokedex.getSpeed());
+            preparedStatement.setInt(4, pokedex.getSpecial_defence());
+            preparedStatement.setInt(5, pokedex.getSpecial_attack());
+            preparedStatement.setInt(6, pokedex.getDefence());
+            preparedStatement.setInt(7, pokedex.getAttack());
+            preparedStatement.setInt(8, pokedex.getHp());
+            preparedStatement.setString(9, pokedex.getPrimary_type());
+            preparedStatement.setString(10, pokedex.getSecondary_type());
+
+            preparedStatement.executeUpdate();
+
+        }catch(SQLException SQLE){
+            System.out.println("Could not add pokemon.");
+            SQLE.printStackTrace();
+        }
+    }
+    public void updatePokedex(Pokedex pokedex){
+        try{
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            final String UPDATE_QUERY = "UPDATE pokemon SET name = ?, speed = ?, special_defence = ?, " +
+                                        "special_attack = ?, defence = ?, attack = ?, hp = ?, primary_type = ?, " +
+                                        "secondary_type = ? WHERE pokedex_number = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+            String name = pokedex.getName();
+            int speed = pokedex.getSpeed();
+            int special_defence = pokedex.getSpecial_defence();
+            int special_attack = pokedex.getSpecial_attack();
+            int defence = pokedex.getDefence();
+            int attack = pokedex.getAttack();
+            int hp = pokedex.getHp();
+            String primary_type = pokedex.getPrimary_type();
+            String secondary_type = pokedex.getSecondary_type();
+            int pokedex_number = pokedex.getPokedex_number();
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, speed);
+            preparedStatement.setInt(3, special_defence);
+            preparedStatement.setInt(4, special_attack);
+            preparedStatement.setInt(5, defence);
+            preparedStatement.setInt(6, attack);
+            preparedStatement.setInt(7, hp);
+            preparedStatement.setString(8, primary_type);
+            preparedStatement.setString(9, secondary_type);
+            preparedStatement.setInt(10, pokedex_number);
+
+            preparedStatement.executeUpdate();
+
+        }catch(SQLException SQLE){
+            System.out.println("Could not update pokedex.");
+            SQLE.printStackTrace();
+        }
+    }
+
+    public Pokedex findPokemonByNumber(int pokedex_number) {
+        Pokedex pokedex = new Pokedex();
+        pokedex.setPokedex_number(pokedex_number);
+        try{
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            final String FIND_POKEMON_QUERY = "SELECT * FROM pokemon WHERE pokedex_number = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_POKEMON_QUERY);
+
+            preparedStatement.setInt(1, pokedex_number);
+
+            //Make a result set for return statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //Return result
+            resultSet.next();
+
+            String name = resultSet.getString(2);
+            int speed = resultSet.getInt(3);
+            int special_defence = resultSet.getInt(4);
+            int special_attack = resultSet.getInt(5);
+            int defence = resultSet.getInt(6);
+            int attack = resultSet.getInt(7);
+            int hp = resultSet.getInt(8);
+            String primary_type = resultSet.getString(9);
+            String secondary_type = resultSet.getString(10);
+
+            pokedex.setName(name);
+            pokedex.setSpeed(speed);
+            pokedex.setSpecial_defence(special_defence);
+            pokedex.setSpecial_attack(special_attack);
+            pokedex.setDefence(defence);
+            pokedex.setAttack(attack);
+            pokedex.setHp(hp);
+            pokedex.setPrimary_type(primary_type);
+            pokedex.setSecondary_type(secondary_type);
+
+        }catch(SQLException SQLE){
+            System.out.println("Could not find pokemon.");
+            SQLE.printStackTrace();
+        }
+        return pokedex;
+    }
+
+    public void deletePokemonNumber(int pokedex_number) {
+        try{
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            final String DELETE_POKEMON = "DELETE FROM pokemon WHERE pokedex_number = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_POKEMON);
+            preparedStatement.setInt(1, pokedex_number);
+
+            preparedStatement.executeUpdate();
+
+        }catch(SQLException SQLE){
+            System.out.println("Could not delete pokemon.");
+            SQLE.printStackTrace();
+        }
+    }
 }
